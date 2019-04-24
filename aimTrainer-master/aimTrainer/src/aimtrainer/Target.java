6d9c1e387d78f5243e24628f8c90fb52520fc077
sfaisal25 <saad.faisal25@gmail.com>
2019-04-24 10:07:23 -0400

    image

--- aimTrainer-master/aimTrainer/src/aimtrainer/Target.java
+++ aimTrainer-master/aimTrainer/src/aimtrainer/Target.java
@@ -3,12 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package aimtrainer;
+
+import javax.swing.*;
+import java.awt.Image;
 
 /**
  *
  * @author Saad Faisal
  */
 public class Target {
+    private int x;
+    private int y;
+    private String path = "target.png";
+    
+    
+    public Target() {
+        
+    }
     
 }
