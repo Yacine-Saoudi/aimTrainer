96b7c4317c721a4de75cb1f77e7ab61701dfbe2b
DESKTOP-OJ4HPU1\Saad Faisal <saad.faisal25@gmail.com>
2019-04-24 22:05:17 -0400

    Added target class methods

--- aimTrainer-master/aimTrainer/src/aimtrainer/Target.java
+++ aimTrainer-master/aimTrainer/src/aimtrainer/Target.java
@@ -6,7 +6,12 @@
 
 package aimtrainer;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Target {
@@ -14,11 +19,13 @@ public class Target {
     private final int y;
     public static String path = "target.png";
     private int radius = 0;
+    JLabel label = new JLabel();
     
     public Target() {
         Random rand = new Random();
         x = rand.nextInt(761) + 20;
-        y = rand.nextInt(361) + 20;
+        y = rand.nextInt(561) + 20;
+        label.setBounds(x,y,radius,radius);
     }
     
     public int getX() {
@@ -33,8 +40,22 @@ public class Target {
         return radius++;
     }
     
-    //public JLabel returnLabel(){
+    public void incRad() {
+        radius++;
+    }
+    
+    public JLabel getLabel() throws IOException{
+        label.setBounds(x-radius,y-radius,radius,radius);
+        BufferedImage img = null;
+        img = ImageIO.read(new File(path));
+       
+        Image dimg = img.getScaledInstance(label.getWidth(), label.getHeight(),
+            Image.SCALE_SMOOTH);
+        ImageIcon image = new ImageIcon(dimg);
         
-    //}
+        label.setIcon(image);
+        
+        return label;
+    }
     
 }
